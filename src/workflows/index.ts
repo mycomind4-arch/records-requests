29350dea3cec5ff09bd256a1d106b945e5e32186
mycomind4-arch <mycomind4@gmail.com>
2026-08-25 00:09:01 -0700

    feat: register government communications workflow

--- src/workflows/index.ts
+++ src/workflows/index.ts
@@ -1,12 +1,14 @@
 import { codeEnforcementRecordsWorkflow } from './code-enforcement-records'
 import { policeRecordsWorkflow } from './police-records'
 import { propertyPermitRecordsWorkflow } from './property-permit-records'
+import { governmentCommunicationsRecordsWorkflow } from './government-communications-records'
 import type { RecordsWorkflow } from '../workflow-factory'
 
 export const recordsWorkflows: readonly RecordsWorkflow[] = [
   codeEnforcementRecordsWorkflow,
   policeRecordsWorkflow,
   propertyPermitRecordsWorkflow,
+  governmentCommunicationsRecordsWorkflow,
 ]
 
 const workflowMap = new Map(recordsWorkflows.map((workflow) => [workflow.id, workflow]))
